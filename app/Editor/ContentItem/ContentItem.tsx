b970e721f219afdbbe7dbbeda6886c58d25f9ac2
petrhavel2 <petr.havel2@gmail.com>
2023-05-21 13:40:57 +0200

    select nested lesson when current one does not have content

--- app/Editor/ContentItem/ContentItem.tsx
+++ app/Editor/ContentItem/ContentItem.tsx
@@ -16,12 +16,28 @@ export const ContentItem: React.FC<TProps> = ({lessons, level, baseIndex, handle
     return (baseIndex ? `${baseIndex}.` : '') + index;
   }
 
+  const handleClick = (lesson: TLesson) => {
+
+    let slug: string;
+
+    if (lesson.content) {
+      slug = lesson.slug;
+    } else if (lesson.children?.length) {
+      // TODO - handle when nested lesson does not have content
+      slug = lesson.children[0].slug;
+    } else {
+      return;
+    }
+
+    handleSelectLesson(slug);
+  };
+
   return (
     <S.Wrapper level={level}>
       {lessons.map((item, index) => (
         <div key={item.name}>
 
-          <S.ActiveLink onClick={() => handleSelectLesson(item.slug)}>
+          <S.ActiveLink onClick={() => handleClick(item)}>
             <S.Row>
               <S.Number>{createIndex(index + 1, baseIndex)}</S.Number>
               <S.Name level={level}>{item.name}</S.Name>
