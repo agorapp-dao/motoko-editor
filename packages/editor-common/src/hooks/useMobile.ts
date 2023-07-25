1ed16ae8bb18ca56b60d20a6bef7fae4430fdf89
petrhavel2 <petr.havel2@gmail.com>
2023-07-25 17:08:45 +0200

    fix ssr for mobile version

--- packages/editor-common/src/hooks/useMobile.ts
+++ packages/editor-common/src/hooks/useMobile.ts
@@ -1,10 +1,14 @@
 import { useWindowWidth } from '@react-hook/window-size';
 import { MOBILE_TRIGGER_WIDTH } from '../constants';
+import { useEffect, useState } from 'react';
 
 export const useMobile = () => {
   const width = useWindowWidth();
+  const [mobile, setMobile] = useState(false);
 
-  const mobile = width < MOBILE_TRIGGER_WIDTH;
+  useEffect(() => {
+    setMobile(width < MOBILE_TRIGGER_WIDTH);
+  }, [width]);
 
   return { mobile };
 };
