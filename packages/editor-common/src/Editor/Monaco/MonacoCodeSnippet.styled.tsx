f4bf9f192d7e5cebfa565ca32932dcc58d9eb298
Roman Masek <romario333@gmail.com>
2023-11-06 09:48:43 +0100

    fix code size in editor

--- packages/editor-common/src/Editor/Monaco/MonacoCodeSnippet.styled.tsx
+++ packages/editor-common/src/Editor/Monaco/MonacoCodeSnippet.styled.tsx
@@ -9,6 +9,7 @@ export const Block = styled.div`
   margin: 1rem 0;
   padding: 1rem;
   overflow-y: scroll;
+  font-size: 0.9em;
 `;
 
 export const Inline = styled.code`
@@ -17,6 +18,7 @@ export const Inline = styled.code`
   border-radius: 5px;
   padding: 0.1rem 0.3rem;
   display: inline-block;
+  font-size: 0.9em;
 `;
 
 export const CopyToClipBoard = styled.span`
